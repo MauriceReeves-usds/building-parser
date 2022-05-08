06a6f43a32d888ca1bfa93896003cb9a132049d8
Maurice Reeves <maurice.e.reeves@omb.eop.gov>
2022-05-08 12:21:02 -0400

    Reorganized tests

--- src/__tests__/parser-tests.js
+++ src/__tests__/parser-tests.js
@@ -2,8 +2,6 @@
  * Main test runner
  */
 const {Parser} = require('../Parser');
-const {Tokenizer} = require('../Tokenizer');
-
 
 test("Test parsing numeric literal", () => {
     // arrange
@@ -168,44 +166,3 @@ test("Test parsing multi line program with comments", () => {
     });
 });
 
-// testing the tokenizer
-test("Test hasMoreTokens with empty string", () => {
-    // arrange
-    const tokenizer = new Tokenizer();
-    // act
-    tokenizer.init('');
-    // assert
-    expect(tokenizer.hasMoreTokens()).toBe(false);
-});
-
-test("Test hasMoreTokens with non-empty string", () => {
-    // arrange
-    const tokenizer = new Tokenizer();
-    // act
-    tokenizer.init(`42`);
-    // assert
-    expect(tokenizer.hasMoreTokens()).toBe(true);
-});
-
-test("Test getNextToken with empty string", () => {
-    // arrange
-    const tokenizer = new Tokenizer();
-    // act
-    tokenizer.init('');
-    // assert
-    expect(tokenizer.getNextToken()).toBe(null);
-});
-
-test("Test getNextToken with non-empty string", () => {
-    // arrange
-    const tokenizer = new Tokenizer();
-    // act
-    tokenizer.init(`42`);
-    // assert
-    expect(tokenizer.getNextToken()).toStrictEqual(
-        {
-            type: 'NUMBER',
-            value: '42'
-        }
-    );
-});
