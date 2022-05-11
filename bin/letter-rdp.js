ab781e5267f17dcd24f6d27a9d954c5810073f30
Maurice Reeves <maurice.e.reeves@omb.eop.gov>
2022-05-11 10:58:21 -0400

    Adds executable and sample file

--- bin/letter-rdp.js
+++ bin/letter-rdp.js
@@ -5,15 +5,18 @@ const { readFileSync } = require('fs');
 const { Parser } = require('../src/Parser');
 
 function main(argv) {
-  const [_node, _path, mode, exp] = argv;
+  const [,, mode, exp] = argv;
   const parser = new Parser();
 
   let ast = null;
 
+  // pass in an arbitrary piece of code and let it compute the AST:
+  // ./bin-letter.rdp.js -e 'let x = 2 + 2;'
   if (mode === '-e') {
     ast = parser.parse(exp);
   }
 
+  // run example file like this: ./bin/letter-rdp.js -f ./src/__tests__/sample-file.lt
   if (mode === '-f') {
     const src = readFileSync(exp, 'utf-8');
     ast = parser.parse(src);
